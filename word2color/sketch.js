59b5f4f8ac970f0d07b43e97ae8cc32ea2c73aee
tripzilch <tripzilch@gmail.com>
2021-04-11 22:09:53 +0200

    updates

--- word2color/sketch.js
+++ word2color/sketch.js
@@ -9,7 +9,7 @@ document.body.addEventListener('keyup', ev => {
     xorshift128_seed(seed);
     let sum = [];
     for (let i = 0; i < 230; i++) sum.push(RNG()); 
-    document.body.style.background = hsl(RNG() * 360, 50 + 50 * RNG(), 50 + 50 * RNG());
+    document.body.style.background = hsl(RNG() * 360, 25 + 75 * RNG(), 50 + 50 * RNG());
   }, 500);
 });
 
