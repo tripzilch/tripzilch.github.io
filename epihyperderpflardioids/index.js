6e1ba557c5accd23012b52d5f0e3b20b38d87d13
ritz <pseudoscalar@gmail.com>
2024-04-28 16:40:30 +0200

    updates

--- epihyperderpflardioids/index.js
+++ epihyperderpflardioids/index.js
@@ -1,133 +1,113 @@
-const default_opts = {
-  fps: 1,
-  N: 200000
-};
-function hash_opts(defaults={}) {
-  const ifnum = s => {
-    const x = Number(s);
-    return isNaN(x) ? decodeURIComponent(s) : x;
+console.table(opts={
+  N:2e5,
+  fps:1,
+  seed:'seed'+Date.now(),
+  ...Object.fromEntries(new URL(location).searchParams)
+});
+S=Uint32Array.of(9,7,5,3);
+R=(a=1)=>a*(a=S[3],S[3]=S[2],S[2]=S[1],a^=a<<11,S[0]^=a^a>>>8^(S[1]=S[0])>>>19,S[0]/2**32);
+[...opts.seed+"HYPERDERP"].map(c=>R(S[3]^=c.charCodeAt()*S[0]));
+RS=(a=1)=>R()<.5?a:-a;
+RA=a=>a[R(a.length)|0];
+T=a=>R(a)-R(a);
+
+({min,max,PI,sin,cos,abs:B}=Math);TAU=PI*2;
+F=(N,f)=>[...Array(N)].map((_,i)=>f(i));
+gcd=(a,b)=>b?a>b?gcd(b,a%b):gcd(a,b%a):a;
+A=([x,y,z],[a,b,c],t)=>[x+a*t,y+b*t,z+c*t];
+N=([x,y,z],m=1e-99+(x*x+y*y+z*z)**.5)=>[x/m,y/m,z/m];
+rot=(x,y,c,s)=>[c*x+s*y,c*y-s*x];
+SM=(a,b,x)=>(x-=a,x/=b-a)<0?0:x>1?1:x*x*(3-2*x);
+mix=([x,y,z=0,w=0],[r,g,b=0,a=0],t,s=1-t)=>[x*s+r*t,y*s+g*t,z*s+b*t,w*s+a*t];
+linearRGB=col=>col.map(c=>c**(1/2.2));
+
+gepi=()=>{
+  let x=[],f,g,y,ii=0,a0v,s0,s1,s2,s3,wa0,f0,f1,f2,f3;
+  for(;ii<9999&&(!x[0]);f=1+R(9)|0,g=1+R(f)|0,h=f+g,x=[2,3,4,5,6,7,8,9,10].filter(d=>h%d<1&&gcd(f,gcd(g,d))==1),ii++);y=RA(x);
+  if(ii>=9999)throw 'ARAARARARR';
+  // let x=[],f,g,y;
+  // for(;!x[0]||f%2+g%2<1||f%3+g%3<1;f=2+R(7)|0,g=1+R(f-1)|0,h=f+g,x=[2,3,4,5,6,7].filter(d=>h%d<1));
+  // y=RA(x);
+  let J=n=>RS(1+R(n)|0)*y;
+  return {
+    p0:[f0=J(3+R(3)),R(),R(),s0=RS(.1+R(.2+R(.5)))/y],
+    p1:[f1=J(3+R(3)),R(),R(),s1=RS(.1+R(.2+R(.5)))/y],
+    a0:[f2=J(3+R(3)),R(),a0v=.3+R(.4),s2=RS(.1+R(.2+R(.4)))],
+    a1:[f3=J(3+R(3)),R(),1-a0v,s3=RS(.1+R(.2+R(.4)))],
+    wap0:[J(3+R(2)),R(),R(),R(R())],
+    wap1:[J(3+R(2)),R(),R(),R(R())],
+    wap2:[J(3+R(2)),R(),R(),R(R())],
+    wap3:[J(3+R(2)),R(),R(),R(R())],
+    wwf:J(3+R(2)),wwg:J(3+R(2)),
+    wp:R(),wq:R(),
+    wa0:wa0=.07+.1*R(.3+R(.7)),
+    wf:((4+B(s0*f0)+B(s1*f1)+B(s2*f2)+B(s3*f3))*30|0)*h,
+    WN:RA([3,4,999]),
+    f,g,y,h,
+    mr:.95/(1+B(s2)+B(s3)),
+    sens:{
+      str:0,
+      p:F(99,_=>N([T()-T(),T()-T(),T()-T()])),
+      j:F(99,_=>.4*RS(1+R(3))),
+      f:F(99,_=>.002*RS(1+R(4))),
+    }
   }
-  const h = location.hash.substring(1);
-  const res = Object.assign({}, defaults);
-  for (const key_value of h.split('&')) {
-    let [k, v] = key_value.split('=');
-    if (v === undefined) { v = true; } 
-    else if (v.includes(',')) { v = v.split(',').map(ifnum); } 
-    else { v = ifnum(v); }
-    res[k] = v;
-  }
-  return res;
 }
-const opts = hash_opts(default_opts);
-//info.hidden=!opts.fps;
+ep=gepi();
 
-S=Uint32Array.from([9,7,n=t=5,3]);
-R=(a=1)=>a*(t=S[3],S[3]=S[2],S[2]=S[1],S[1]=n=S[0],t^=t<<11,S[0]^=(t^t>>>8)^(n>>>19),S[0]/2**32);
-[...Date.now()+'Kallisti'].map(c=>R(S[3]^=c.charCodeAt()*23205));
-({min,max,PI}=Math);TAU=PI*2;
-L=(N,f)=>[...Array(N)].map((_,i)=>f(i));
-L(9,R);
-
-x=[];for(;x.length<1||f%2+g%2<1||f%3+g%3<1;f=2+R(6)|0,g=1+R(f-1)|0,h=f+g,x=[2,3,5,7].filter(d=>h%d<1));y=x[R(x.length)|0];
-
-//!= && f%3
-// 2 1
-// 3 1 2
-// 4 1 2 3
-// 5 1 2 3 4
-// 6 1 2 3 4 5
-// 7 1 2 3 4 5 6
-//f=4;g=2;y=2;
-console.table({freq0:f,freq1:g,symmetry:`${x} (${y})`});
-I=n=>(1+R(n)|0)*y*(R()<.5?1:-1);
-G=(a=.5)=>.1+R(a/y);rs=x=>R()<.5?-x:x;
-TF=(s=1)=>`${R()}+${rs(s)}*(${R()+.5}*t`+(R()<.3?`+${rs(R()+1)}*pow(O(${R(.4)}, ${R()}, .5, .5, t), 16.)`:``)+`)`
-st=0;
 K=`#version 300 es
 precision highp float;
-`+`,S smoothstep,W vec3,V vec2,X vec4,F float
-`.replace(/,/g,`
+`+`~S smoothstep~W vec3~V vec2~X vec4~F float~B abs~U min~G max~L length~N normalize~G max~U min
+`.replace(/~/g,`
 #define `);
 const vc = `uniform V res;
 uniform int NV;
-uniform float time;
+uniform X p0,a0,p1,a1,wap0,wap1,wap2,wap3;
+uniform F f,g,wwf,wwg,wf,wp,wq,wa0,WN,mr;
 out V pos;
 out F size;
 
-float rnd(float p) {
-  V p2 = fract(V(5.6474, 5.5092) * p);
-  p2 += dot(p2.yx, p2.xy + V(21.0522, 14.1077));
-  return fract(p2.x * p2.y * 95.6998);
-}
 #define TAU ${TAU}
 
-V R(float f, float p, float a, float t) {
-  // rotating V with frequency f, phase p, amplitude a
-  // at time t
-  t = (f * t + p) * TAU;
-  return V(cos(t), sin(t)) * a;
+mat2 rot1(float a) {
+    a*=TAU;
+    float s = sin(a), c = cos(a);
+    return mat2(c, -s, s, c);
 }
 
-V P(F f,F p,F a,F s){
-  const F N=${3+R(2)|0};
-  F tf=f*s*N,ti=floor(tf);tf-=ti;ti/=N;
-  return mix(R(1,p,a,ti),R(1,p,a,ti+(1/N)),tf);
+V RO(F f, F p, F a, F t){
+  t=(t*f+p)*TAU;
+  return a*V(sin(t),cos(t));
 }
-
-float O(float f, float p, float v, float d, float t) {
+V RWN(F f, F p, F a, F t, F q){
+  t=(t*f+p)*WN;
+  F i=floor(t);
+  t-=i;i/=WN;
+  return mix(RO(1,q,a,i),RO(1,q,a,i+1/WN),t);
+}
+F OS(F f, F p, F v, F d, F t) {
   // oscillator with frequency f, phase p, mid value v
   // distance d at time t
   return v + d * sin((f * t + p) * TAU);
 }
-
-V pss(float u, float t) {
-  float ps0 = O(${I(6)+y}, ${TF(.5)}, .5-t, .2, u); 
-  float ps = O(${I(6)+y}, ps0, .5, .5, u); 
-  float pb = O(${I(6)+y}, O(${I(6)+y}, ${TF(.5)}, ${TF(.5)}, .2, u), .5, .5, u); 
-  V s = V(ps,1-ps), b=V(pb,1-pb);
-  s*=s*s*s;b*=b*b*b;
-  return .5*s+.5*b;
-}
+F OS(X o, F t) { return OS(o.x,o.y,o.z,o.w,t); }
 
 void main() {
-  float u = float(gl_VertexID) / float(NV);  // goes from 0 to 1
-  // float t = (time+rnd(u*2.71+2.3+fract(time))/30.) * .1;
-  float t = time * .1;
-  const float y = ${y};
-  V ps;
-  
-  // F uf=u*1600,ui=floor(uf),fi=step(.53,pss(u,t));
-  // uf-=ui;
-  // u=mix(u,ui/1600,fi);  
-  // ps=pss(u,t);F ps0=ps;
-  // ps*=ps*ps;
-  // V w=(R(1,0,.15*rnd(ui*.77+3)+.02,uf)+(V(rnd(ui*.34),rnd(ui*.56-1))-.5))*ps*fi/5;
+  float t = float(gl_VertexID) / float(NV);  // goes from 0 to 1
 
-  // ps=pss(u,t);
-  // ps.x*=ps.x;ps*=ps;
-  // V w = R(${150*(y+h)}, 0., .05*ps.x  , u);
+  F w0=OS(wap0,t),w1=OS(wap1,t),w2=OS(wap2,t),w3=OS(wap3,t);
+  F wa=OS(wwf,w0*w1,0,1,t);
+  F wb=OS(wwg,w2*w3,0,1,t);
+  F ww=pow(G(0,wa*wb),3);
+  V p0=RO(f,OS(p0,t),OS(a0,t),t);
+  V p1=RO(-g,OS(p1,t),OS(a1,t),t);
+  V p=p0+p1+RWN(wf,wp,wa0*ww,t,f*t+wq);
 
-  ps=pss(u,t);
-  ps.x*=ps.x*ps.x;
-  V w = ${R()<.4?'R':'P'}(${180*(y+h)}, ${R()}+u, .06*ps.x   , u);
-
-  const X am = normalize(X(${L(4,_=>R())}));
-  float p0 = O(${I(5)}, ${TF()}, ${z=TF(.2)}, am.x * ${G(.5)}, u);
-  float p1 = O(${I(5)}, ${TF()}, -${R()+.5}*(${z}), am.y * ${G(.5)}, u);
-  float a0 = O(${I(5)}, ${TF()}, .4, am.z * ${G(.5)}, u);
-  float a1 = O(${I(5)}, ${TF()}, .4, am.w * ${G(.5)}, u);;
-
-  V p = R(${f}, p0, a0, u) + R(${-g}, p1, a1, u) + w;
-  //const float N = 600.;
-  //float uf = u * N; 
-  //float ui = floor(uf);uf-=ui;ui/=N;
-  //V n = R(1., 0., sqrt(rnd(2.*ui+2.1)), rnd(ui)) + R(1., 0., .2, uf);
-
-  V aspect = res.yx * (.66+${y*.02}) / max(res.x, res.y);
+  V aspect = res.yx * mr / max(res.x, res.y);
   pos = p * aspect;
-  size = 3 + 8*S(0.2, 1, ps.y);
+  size = 2*pow(G(0,-wa*wb),3);
   gl_Position = vec4(pos, 0, 1);
-  gl_PointSize = size;
+  gl_PointSize = 4 + 9*size;
 }`;
 
 const fc = `
@@ -137,16 +117,19 @@ out vec4 outColor;
 
 void main() {
   F d = length(gl_PointCoord*2-1);
-  outColor=X(W(0),clamp((1-d)*size/2,0,1));   // 1-d/f
+  outColor=X(W(0),S(1,size*.4,d));   // 1-d/f
 }`;
   // x = Math.min(Math.max((x - e0) / (e1 - e0), 0), 1);
 //
+// dbg.style.display='none';
 const gl = c.getContext('webgl2',{alpha:false});
 const loc = {};
+er='<pre>'
 const Shader = (typ, src)=>{
   const s=gl.createShader(typ);
   gl.shaderSource(s,K+src.replace(/([^a-zA-Z_0-9.])([0-9]+)(?![.0-9u])/g,'$1$2.').replace(/([0-9.]e-[0-9]+)\./gi,'$1'));
   gl.compileShader(s);
+  (/*console.log(c),*/m=gl.getShaderInfoLog(s))&&(er+=src.split`\n`.map((v,i)=>++i+': '+v).join`\n`+'\n'+m);
   return s;
 }
 
@@ -162,18 +145,20 @@ if (!gl.getProgramParameter(program, gl.LINK_STATUS)) {
   console.log("Link failed:\n", gl.getProgramInfoLog(program));
   console.log("VS LOG:\n", gl.getShaderInfoLog(vs));
   console.log("FS LOG:\n", gl.getShaderInfoLog(fs));
+  document.body.innerHTML=er;console.log(er);
   throw ("Program failed to link (see console for error log).");
 }
 
-const uniforms = 'res NV time'.split(' ');
+const uniforms = ['res','NV',...Object.keys(ep)];
 for (let uni of uniforms) {
   loc[uni] = gl.getUniformLocation(program, uni);
 }
 
 gl.useProgram(program);
-gl.clearColor(1,1,1,1);
-
-const N = opts.N;
+gl.enable(gl.SAMPLE_ALPHA_TO_COVERAGE);
+// gl.enable(gl.SAMPLE_COVERAGE);
+gl.enable(gl.BLEND);
+gl.blendFunc(gl.SRC_ALPHA, gl.ONE_MINUS_SRC_ALPHA);
 
 const res = {};
 function resize() { 
@@ -185,21 +170,101 @@ function resize() {
 }
 resize();
 window.addEventListener('resize', resize);
+froz=0;
+alpha=0;beta=0;gamma=0;
+acs=cos(alpha),asn=sin(alpha),bcs=cos(beta),bsn=sin(beta),gcs=cos(gamma),gsn=sin(gamma);
+rad=TAU/360;
+eori=e => ({alpha,beta,gamma}=e,alpha*=rad,beta*=rad,gamma*=rad,acs=cos(alpha),asn=sin(alpha),bcs=cos(beta),bsn=sin(beta),gcs=cos(gamma),gsn=sin(gamma),froz=1);
+uax=0;
+pal=[
+  [1,.3,.01,1], // orange
+  [1,.7,.01,1], // yellow
+  [.6,1,.01,1], // lime
+  [.2,1,.6,1], // ocean
+  [.1,.5,1,1], // sky
+  [.6,.2,1,1], // berry
+  [1,.15,.6,1], // gum
+];
+NPAL=pal.length;
+bgcol1=bgcol0=3;//R(NPAL)|0;
+needperm=!!(window.DeviceMotionEvent && window.DeviceMotionEvent.requestPermission);
+// dbg.value+=`needperm:${needperm}\n`;
+motionctl=false;
+pepixt=epixt=-1;
+
+c.onclick=e=>{
+  e.preventDefault();
+  // dbg.value+='click\n';
+  if(!motionctl && needperm) {
+    // dbg.value+='requesting permission\n';
+    DeviceMotionEvent.requestPermission();
+    // dbg.value+='adding deviceorientation event\n';
+    window.addEventListener("deviceorientation", eori);
+    motionctl=true;
+  }
+}
+if(!needperm){
+    // dbg.value+='adding deviceorientation event without permission request\n';
+    window.addEventListener("deviceorientation", eori);
+    motionctl=true;  
+}
+c.onpointerdown=e=>{epixt=-1;bgcol0=bgcol1;bgcol1=(bgcol1+RS(2+R(4)|0)+NPAL)%NPAL;};
 
 let frame_count = 0;
 let prev_time = performance.now();
 let fps = 30;
+xf=([x,y,z])=>(
+  [x,y]=rot(x,y,acs,asn),
+  [y,z]=rot(y,z,bcs,bsn),
+  [z,x]=rot(z,x,gcs,gsn),[x,y,z]
+)
+let fqv=F(99,_=>0),fqx=F(99,_=>0),prev_t=-1;
+let fqpq=F(99,_=>[0,0,0]);
+// let fqz=(t,i)=>ep.sens.j[i]*xf(ep.sens.p[i])[0]+t*ep.sens.f[i];
+let fqz=(dt,t,i)=>{
+  let q=xf(ep.sens.p[i]);
+  let f=max(0,ep.sens.str*2-1)*(.4**(dt*9));
+  fqx[i]=fqx[i]*f+q[0]*(1-f);
+  return ep.sens.j[i]*fqx[i] + ep.sens.f[i]*t;
+}
+let fqi=0;
+
 function render(time) {
+  let t=time*.001;
+  if (prev_t<0) prev_t=t;
+  let dt=t-prev_t;
+  prev_t=t;
+  epixt+=dt*15;
+  // let reset=1;
+  if(pepixt<=0&&epixt>0) {ep=gepi()}
+  let fade=SM(-1,1,epixt);
+  let smul=B(fade*2-1);
+  let NPTS = opts.N * smul | 0;
+  ep.sens.str=smul;
+  pepixt=epixt;
   gl.viewport(0, 0, c.width, c.height);
+  gl.clearColor(...linearRGB(mix(pal[bgcol0],pal[bgcol1],fade)));
   gl.uniform2fv(loc.res, [res.x, res.y]);
-  gl.uniform1i(loc.NV, N);
-  gl.uniform1f(loc.time, time * .001);
-gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
-gl.enable(gl.SAMPLE_ALPHA_TO_COVERAGE);
-// gl.enable(gl.SAMPLE_COVERAGE);
-gl.enable(gl.BLEND);
-gl.blendFunc(gl.SRC_ALPHA, gl.ONE_MINUS_SRC_ALPHA);
-  gl.drawArrays(gl.POINTS, 0, N);
+  gl.uniform1i(loc.NV, NPTS);
+  fqi=0;
+  [...Object.entries(ep)].map(([k,v])=>{
+// p0[1],p0[2],p1[0],p1[2],a0[1],a1[1],wap0[1],wap1[1],wp,wq
+    if(v[0]){
+      v=[...v];
+      if(['p0','p1','a0','a1','wap0','wap1'].includes(k)){
+        v[1] += fqz(dt,t,fqi++);
+      }
+      if(k=='p0'||k=='p1'){ v[2] += fqz(dt,t,fqi++)/3; }
+      gl.uniform4fv(loc[k],v);
+    } else {
+      if(k=='wp'||k=='wq'){ v += fqz(dt,t,fqi++); }
+      if(k=='mr') v*=smul;
+      gl.uniform1f(loc[k],v);
+    }
+  });
+  // console.log(fqi);
+  gl.clear(gl.COLOR_BUFFER_BIT);
+  gl.drawArrays(gl.POINTS, 0, NPTS);
 
 //   gl.uniform1f(loc.time, time * .001 + 0.1);
 // gl.enable(gl.BLEND);
@@ -207,10 +272,10 @@ gl.blendFunc(gl.SRC_ALPHA, gl.ONE_MINUS_SRC_ALPHA);
 //   gl.drawArrays(gl.POINTS, 0, N);
 
   frame_count++;
-  let dt = time - prev_time;
-  fps = fps * 0.9 + (1000/dt)*0.1;
+  fps = fps * 0.9 + (dt>0?(1/dt)*0.1:0);
+  document.title=fps.toFixed(1)+'fps';
   //info.innerHTML = fps.toFixed(1)+'fps';
-  prev_time = time;
   requestAnimationFrame(render);
 }
 requestAnimationFrame(render);
+
